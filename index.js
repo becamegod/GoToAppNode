ee3be2ed0b6758df35b568eea74ee6e2e2f8ee18
Thành <37612705+becamegod@users.noreply.github.com>
2022-08-18 11:38:07 +0700

    Add type into message's payload

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 const admin = require("firebase-admin");
 const serviceAccount = JSON.parse(process.env.adminsdk);
 const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
+
+// constants
+const type = require('./alertType');
 
 // init
 admin.initializeApp({
@@ -11,9 +15,9 @@ admin.initializeApp({
 
 // listen realtime database (booking)
 let firstTime = true;
-const rt = admin.database();
-const bookingRef = rt.ref("booking");
-const availableDriversRef = rt.ref('availableDrivers');
+const realtime = admin.database();
+const bookingRef = realtime.ref("booking");
+const availableDriversRef = realtime.ref('availableDrivers');
 bookingRef.on('value', (snapshot) => {
   if (firstTime) {
     firstTime = false;
@@ -23,8 +27,8 @@ bookingRef.on('value', (snapshot) => {
   console.log('new booking change: ', change);
 
   // get customer's info
-  const fs = admin.firestore();
-  fs.collection('users').doc(change.customerId).get()
+  const firestore = admin.firestore();
+  firestore.collection('users').doc(change.customerId).get()
     .then(document => {
 
       // add to "change"
@@ -39,14 +43,7 @@ bookingRef.on('value', (snapshot) => {
       else {
         /// [FCM]        
         // get available drivers
-        availableDriversRef.once('value')
-          .then(snapshot => {
-            const drivers = snapshot.val();
-            const tokens = Object.values(drivers);
-            console.log('tokens: ', tokens);
-            if (tokens.length > 0) notifyDrivers(tokens, change);
-          })
-          .catch(error => console.log(error));
+        getAvailableDrivers((tokens) => notifyDrivers(tokens, change, type.clientFound));
 
         // get all device tokens from firestore db
         // fs.collection('users').where('accountType', '==', 'Driver').get()
@@ -63,13 +60,14 @@ bookingRef.on('value', (snapshot) => {
 
 });
 
-const notifyDrivers = (tokens, payload) => {
+const notifyDrivers = (tokens, payload, type) => {
 
   let content = {};
   content.id = uuidv4();
   content.channelKey = 'basic_channel';
   content.title = 'Có khách ở gần bạn';
   content.body = 'Đã tìm thấy khách hàng đang tìm xe, hãy mau đến đón.';
+  payload.type = type;
   content.payload = payload;
 
   const message = {
@@ -94,6 +92,46 @@ const notifyDrivers = (tokens, payload) => {
         console.log('message sent')
       }
     });
+}
+
+// [DEBUG] read bookingRes handled keys
+const handledFile = 'handled.txt';
+let handled = [];
+fs.readFile(handledFile, 'utf8', (error, data) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  handled = data.split('\r\n');
+
+  // listen for bookingRespond
+  const bookingResRef = realtime.ref('bookingRespond');
+  bookingResRef.on('child_added', snapshot => {
+
+    // filter old entries
+    if (snapshot.key in handled) return;
+    fs.appendFile(handledFile, snapshot.key + '\n', err => { if (err) console.error(err) });
+
+    // real handle
+    const respond = snapshot.val();
+    console.log('new booking respond: ', snapshot.key, respond);
+    getAvailableDrivers((tokens) => {
+      const otherTokens = tokens.filter(element => element != respond.driverId);
+      // TODO: FCM
+      notifyDrivers(tokens, {});
+    });
+  });
+});
+
+const getAvailableDrivers = (callback) => {
+  availableDriversRef.once('value')
+    .then(snapshot => {
+      const drivers = snapshot.val();
+      const tokens = Object.values(drivers);
+      console.log('tokens: ', tokens);
+      if (tokens.length > 0) callback(tokens);
+    })
+    .catch(error => console.log(error));
 }
 
 console.log('server started...');
